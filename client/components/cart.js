84e1eb795d422a05efe595426f0973385228d685
Richard Ke <richardke2@gmail.com>
2020-08-04 18:44:16 -0400

    initial cart page

---
+++ client/components/cart.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { fetchCart } from '../store/user'
+
+/**
+ * COMPONENT
+ */
+
+export class Cart extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchCart(this.props.user.id)
+    }
+
+    render() {
+        <div>
+            <h1>Cart:</h1>
+            {this.props.cart.map(item => <div>{item}</div>)}
+        </div>
+    }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+    return {
+        cart: state.cart,
+        user: state.user
+    }
+}
+
+const mapDispatch = dispatch => {
+    return {
+        fetchCart: (id) => dispatch(fetchCart(id))
+    }
+}
+
+export default connect(mapState,mapDispatch)(Cart)
+
+/**
+ * PROP TYPES
+ */
+UserHome.propTypes = {
+    email: PropTypes.string
+}
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+const Cart = require('./cart')
+const Product = require('./product')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +15,12 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+Cart.belongsTo(User);
+User.hasOne(Cart);
+Product.belongsToMany(Cart);
+Cart.belongsToMany(Product);
+
 module.exports = {
   User
 }
