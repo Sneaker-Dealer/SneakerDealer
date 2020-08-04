1350502128929203de1a44900cbc8cc8d1edec98
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-08-04 14:51:24 -0400

    completed redux all-sneakers store

--- client/app.js
+++ client/app.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {fetchSneakers} from './store'
+import {connect} from 'react-redux'
 
 import {Navbar} from './components'
 import Routes from './routes'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.props.getSneakers()
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    )
+  }
 }
 
-export default App
+const mapDispatch = dispatch => {
+  return {getSneakers: () => dispatch(fetchSneakers())}
+}
+
+const mapState = state => {
+  return {
+    sneakers: state.sneakers
+  }
+}
+
+export default connect(mapState, mapDispatch)(App)
